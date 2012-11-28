fc838d70cf5e358ce837f8b62b6af83f09bbd94e
Paul Sutton <suttonpd@gmail.com>
2012-11-28 13:14:43 +0000

    Minor patch for kissfft.hh - avoid variable-length array of non-POD types error

--- lib/generic/kissfft/kissfft.hh
+++ lib/generic/kissfft/kissfft.hh
@@ -264,7 +264,7 @@ class kissfft
             cpx_type * twiddles = &_twiddles[0];
             cpx_type t;
             int Norig = _nfft;
-            cpx_type scratchbuf[p];
+            cpx_type* scratchbuf = new cpx_type[p];
 
             for ( u=0; u<m; ++u ) {
                 k=u;
@@ -287,6 +287,7 @@ class kissfft
                     k += m;
                 }
             }
+            delete [] scratchbuf;
         }
 
         int _nfft;
